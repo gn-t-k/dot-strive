6cf54b7ff96cdf471b249d389db66b9645bf21ee
gn-t-k <gntk1995422@gmail.com>
2023-12-21 16:52:26 +0900

    ダイアログ開くようにした

--- app/routes/trainees.$traineeId.muscles._index/route.tsx
+++ app/routes/trainees.$traineeId.muscles._index/route.tsx
@@ -10,6 +10,7 @@ import { validationError } from 'remix-validated-form';
 import { getAuthenticator } from 'app/features/auth/get-authenticator.server';
 import { validateMuscle } from 'app/features/muscle';
 import { MuscleForm, validator } from 'app/features/muscle/muscle-form';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from 'app/ui/alert-dialog';
 import { Button } from 'app/ui/button';
 import { Card, CardContent, CardDescription, CardHeader } from 'app/ui/card';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from 'app/ui/dropdown-menu';
@@ -74,52 +75,66 @@ const Page: FC = () => {
             return (
               <li key={muscle.id}>
                 <Card className="relative">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger className="absolute right-2 top-2" asChild>
-                      <Button size="icon" variant="ghost">
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuGroup>
-                        <DropdownMenuItem onClick={onClickEdit(muscle.id)}>
-                          <Edit className="mr-2 h-4 w-4" />
-                          編集
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                          <Trash2 className="mr-2 h-4 w-4" />
-                          削除
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                  <CardHeader>
-                    {
-                      editing === muscle.id ? (
-                        <div className="flex items-end space-x-2">
-                          <MuscleForm
-                            method="post"
-                            actionProps={{ type: 'update', muscleId: muscle.id }}
-                            defaultValues={{ name: muscle.name }}
-                            onSubmit={() => setEditing('')}
-                            className="grow"
-                          />
-                          <Button
-                            onClick={onClickCancel}
-                            variant="secondary"
-                            className="grow-0"
-                          >
-                            キャンセル
-                          </Button>
-                        </div>
-                      ) :
-                        <Heading level={2}>{muscle.name}</Heading>
-                    }
-                  </CardHeader>
-                  <CardContent>
-                    <Heading level={3} size="sm">今週のセット数</Heading>
-                    <p>coming soon</p>
-                  </CardContent>
+                  <AlertDialog>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="absolute right-2 top-2" asChild>
+                        <Button size="icon" variant="ghost">
+                          <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuGroup>
+                          <DropdownMenuItem onClick={onClickEdit(muscle.id)}>
+                            <Edit className="mr-2 h-4 w-4" />
+                            編集
+                          </DropdownMenuItem>
+                          <DropdownMenuItem>
+                            <AlertDialogTrigger className='flex'>
+                              <Trash2 className="mr-2 h-4 w-4" />
+                              削除
+                            </AlertDialogTrigger>
+                          </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                    <CardHeader>
+                      {
+                        editing === muscle.id ? (
+                          <div className="flex items-end space-x-2">
+                            <MuscleForm
+                              method="post"
+                              actionProps={{ type: 'update', muscleId: muscle.id }}
+                              defaultValues={{ name: muscle.name }}
+                              onSubmit={() => setEditing('')}
+                              className="grow"
+                            />
+                            <Button
+                              onClick={onClickCancel}
+                              variant="secondary"
+                              className="grow-0"
+                            >
+                              キャンセル
+                            </Button>
+                          </div>
+                        ) :
+                          <Heading level={2}>{muscle.name}</Heading>
+                      }
+                    </CardHeader>
+                    <CardContent>
+                      <Heading level={3} size="sm">今週のセット数</Heading>
+                      <p>coming soon</p>
+                    </CardContent>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>部位の削除</AlertDialogTitle>
+                        <AlertDialogDescription>部位を削除しますか？</AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>キャンセル</AlertDialogCancel>
+                        <AlertDialogAction>削除</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </Card>
               </li>
             );
